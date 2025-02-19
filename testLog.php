9caddf47c982f0a86d52c36e67edda024dab530b
Russell Purinton <russell.purinton@icloud.com>
2025-02-19 02:20:51 +0000

    Updated 2025-02-19

--- testLog.php
+++ testLog.php
@@ -4,13 +4,24 @@ require __DIR__ . '/vendor/autoload.php';
 
 use RPurinton\Log;
 
-Log::install();
-
-Log::trace('This is a trace message');
-Log::debug('This is a debug message');
-Log::info('This is an info message');
-Log::warn('This is a warning message');
+echo "Testing with webhook...\n";
+Log::init();
 Log::error('This is an error message', ['foo' => 'bar']);
 
-// force an error
+echo "Testing log file...\n";
+$log_file = __DIR__ . '/test.log';
+putenv("LOG_FILE=$log_file");
+Log::init();
+Log::error('This is an error message', ['foo' => 'bar']);
+echo(file_get_contents($log_file));
+@unlink($log_file);
+
+echo "Testing console log...\n";
+putenv("LOG_FILE");
+Log::init();
+Log::error('This is an error message', ['foo' => 'bar']);
+
+echo "Testing Error Handling...\n";
+Log::install();
+$foo = $bar;
 $foo = 1 / 0;
