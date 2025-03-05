1725fe09dc344b0c11d3f716577e5ec639d07bb4
Ash <ashcordbot@gmail.com>
2025-03-05 21:27:51 +0000

    Version 1.0.33 - 2025-03-05

--- testLog.php
+++ testLog.php
@@ -11,14 +11,6 @@ function testLogging()
     Log::init();
     Log::error('This is an error message', ['foo' => 'bar']);
 
-    echo "Testing log file...\n";
-    $log_file = __DIR__ . '/test.log';
-    putenv("LOG_FILE=$log_file");
-    Log::init();
-    Log::error('This is an error message', ['foo' => 'bar']);
-    echo (file_get_contents($log_file));
-    @unlink($log_file);
-
     echo "Testing console log...\n";
     putenv("LOG_FILE=php://stdout");
     Log::init();
